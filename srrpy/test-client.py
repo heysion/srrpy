496595320146b95f4986c264eae98f1f39b078c1
heysion <heysion@deepin.com>
2019-02-18 17:43:21 +0800

    update client  Changes to be committed: 	modified:   srrpy/test-client.py

--- srrpy/test-client.py
+++ srrpy/test-client.py
@@ -2,13 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import redis
-from templates import TemplatesInterface as testtf
+from templates import TemplatesInterface
 from srr import Client
+import logging
+import sys
+
+__meta__data__ = '''
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+
+{% for mod in import_modules %}
+import {{ mod }}
+{% endfor %}
+
+def run_test(a,b):
+    print("a + b = %d"%(a+b))
+
+run_test({% for item in list_args -%} {{ item[0] }} = {{ item[1] }} {%- if loop.index == list_args|length -%} {%- else %} , {% endif %} {%- endfor %})
+'''
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+class MyTemplate(TemplatesInterface):
+    name = "mytemplate"
+    metadata = __meta__data__
+
 name = "test"
 db = redis.Redis()
 codec = "json"
-tp = testtf()
+tp = MyTemplate()
 test_client = Client(db,name,5,codec,tp)
-test_client.call()
+test_client.call(import_modules=["sys","os"],list_args={("a",10),("b",3)})
 print(test_client.wait_result())
 
